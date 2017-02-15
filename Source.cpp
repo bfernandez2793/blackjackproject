9bd7ef41d3d0e061fe89c8c72a5c1b57c1b6a4a0
Bryan <hernandjo13@aol.com>
2017-02-14 20:20:34 -0800

    Fifth-Commit
    Implementing a one round game of selecting cards.

--- Source.cpp
+++ Source.cpp
@@ -4,8 +4,22 @@ int main() {
 
 	
 	Player player1(100);
-	player1.print();
-
-
+	Player Dealer;
+	char answer;
+	std::cout << "Would you like to play a game of blackjack?";
+	std::cin >> answer;
+	player1.update_hand();
+	do {
+		std::cout << "Your Cards: \n";
+		player1.print();
+		std::cout << "Would you like another card?\n";
+		std::cin >> answer;
+		if (answer == 'y')
+			player1.update_hand();
+		else
+			Dealer.update_hand();
+	} while (answer == 'y');
+	std::cout << "Dealer's Cards: \n";
+	Dealer.print();
 	return 0;
 }
