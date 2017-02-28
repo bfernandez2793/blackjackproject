62c38554ce1eaef99950b78674be8b9dc5e17bca
Bryan <hernandjo13@aol.com>
2017-02-28 11:08:17 -0800

    Second-Commit(Qt)
    Working simple game between player and dealer

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,7 @@
 #include "ui_mainwindow.h"
 
 
-MainWindow::MainWindow(QWidget *parent):QMainWindow(parent),ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent):QMainWindow(parent),ui(new Ui::MainWindow),player(100)
 {
     setFixedSize(400, 400);
     ui->setupUi(this);
@@ -13,46 +13,56 @@ MainWindow::MainWindow(QWidget *parent):QMainWindow(parent),ui(new Ui::MainWindo
     QObject::connect(ui->hitButton,SIGNAL(clicked(bool)),this,SLOT(setHand()));//update hand button
     QObject::connect(ui->standButton,SIGNAL(clicked(bool)),this,SLOT(setStand()));//update hand button
     QObject::connect(this,SIGNAL(bust()),this,SLOT(setStand()));//if bust dont allow the player to get more cards
-    QObject::connect(this,SIGNAL(hand_changed()),this,SLOT(setOutput()));
-
-
+    QObject::connect(this,SIGNAL(hand_changed()),this,SLOT(setOutput()));//update any change in the players hand
+    QObject::connect(this,SIGNAL(endgame()),this,SLOT(setEndgame()));//output the results of the game
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-    delete player;
-    std::cout << "player released\n";
 }
 void MainWindow::setGame(){
     //initializing the game for one player
-    player = new RandomPlayer(100);
     ui->standButton->setEnabled(true);
     ui->hitButton->setEnabled(true);
+    ui->startButton->setEnabled(false);
     ui->textBrowser->insertPlainText("Player 1:");
     emit hand_changed();
     std::cout << "Start Game\n";
-    std::cout << *player;
+    std::cout << player;
 }
 void MainWindow::setHand()
 {
-    player->update_hand();
+    player.update_hand();
     emit hand_changed();
-    if(player->bust())
-    {
-        ui->textBrowser->insertPlainText("Bust!!");
+    std::cout << player;
+    if(player.bust())
         emit bust();
-    }
-    std::cout << *player;
 }
 void MainWindow::setStand()
 {
-    ui->hitButton->setEnabled(false);
     std::cout << "Set Stand status\n";
+    ui->hitButton->setEnabled(false);
+    dealer.play(player.bust());
+    ui->textBrowser->insertPlainText("\nDealer: " + QString::number(dealer.value_of_hand())) ;
+    std::cout << dealer;
+    emit endgame();
 }
 void MainWindow::setOutput()
 {
-    ui->textBrowser->insertPlainText(QString::number(player->value_of_hand())+" ");
+   ui->textBrowser->insertPlainText(QString::number(player.value_of_hand())+" ");
 }
-
+void MainWindow::setEndgame()
+{
+    if (player.bust())
+        ui->textBrowser->insertPlainText("\nPlayer Bust!! Dealer Wins!!");
+    else if (dealer.bust())
+        ui->textBrowser->insertPlainText("\nDealer Bust!! Player Wins!!");
+    else if (player.value_of_hand() < dealer.value_of_hand())
+        ui->textBrowser->insertPlainText("\nDealer Wins!!");
+    else if (player.value_of_hand() > dealer.value_of_hand())
+        ui->textBrowser->insertPlainText("\nPlayer Wins!!");
+    else
+        ui->textBrowser->insertPlainText("\nTie!!");
+}
 
--- mainwindow.h
+++ mainwindow.h
@@ -19,14 +19,17 @@ public:
 signals:
     void hand_changed();//signal when hand has changed
     void bust();//signal when player has bust
+    void endgame();
 private slots:
-  void setHand();//update hand
-  void setGame();//start game
-  void setOutput();//output to text(temporary)
-  void setStand();//dont not allow player to get more cards
+    void setHand();//update hand
+    void setGame();//start game
+    void setOutput();//output to text(temporary)
+    void setStand();//dont not allow player to get more cards
+    void setEndgame();
 
 private:
-    RandomPlayer *player;
+    RandomPlayer player;
+    Dealer dealer;
     Ui::MainWindow *ui;
     QLCDNumber *mnumberer;
     QTextEdit *text;
